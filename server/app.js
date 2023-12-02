d0499e57a34fbaf816643d7cfd6389ac30edcd57
Diegopie <ishtaracademy@outlook.com>
2023-12-02 14:23:10 -0700

    moved auth routes to own file; need to get middleware working

--- server/app.js
+++ server/app.js
@@ -25,27 +25,6 @@ function loginRequired(req, res, next) {
   }
 }
 
-
-
-app.post('/api/auth', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ where: { email: email } });
-
-  if (user && user.password === password) {
-    req.session.userId = user.userId;
-    res.json({ success: true });
-  } else {
-    res.json({ success: false });
-  }
-});
-
-// Note the `loginRequired` argument passed to the routes below!
-
-app.post('/api/logout', loginRequired, (req, res) => {
-  req.session.destroy();
-  res.json({ success: true });
-});
-
 app.get('/api/ratings', loginRequired, async (req, res) => {
   const { userId } = req.session;
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import movieRouter from "./movie.routes.js";
+import authRoutes from "../auth.routes.js";
 
 const router = Router();
 
 router.use('/api/movies', movieRouter);
+
+router.use('', authRoutes);
 
 export default router;
