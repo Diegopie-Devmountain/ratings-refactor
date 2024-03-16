233d0a8a41c410a94cc389ee262a0032172d2956
Diegopie <ishtaracademy@outlook.com>
2024-03-16 02:30:31 -0600

    finsihed demo ish

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
-const CACHE_NAME = "movie-ratings-cache-v1.5"
+const CACHE_NAME = "movie-ratings-cache-v1.6"
 const filesToCache = [
   "/offline.html",
+  "/siteIcons/mRatings.png"
 ]
 
 // * Handle Events
@@ -13,11 +14,12 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     // Open Cache (promise)
     caches.open(CACHE_NAME)
-    // Use returned cache from promise
-    .then(cache => {
-      return cache.addAll(filesToCache)
-    })
-    .catch(error => console.error("On SW Install, caching error: ", error))
+      // Use returned cache from promise
+      .then(cache => {
+        console.log('files cached');
+        return cache.addAll(filesToCache)
+      })
+      .catch(error => console.error("On SW Install, caching error: ", error))
   );
 
   // Demo then add
@@ -28,4 +30,46 @@ self.addEventListener("install", (event) => {
 self.addEventListener("activate", event => {
   // This will let us handle what to do once it is active and being used
   console.log("Service worker activated");
+  
+  event.waitUntil(
+    caches
+        .keys()
+        .then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME) {
+                        console.log("Deleting Key: ", key);
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })
+        .catch(err => console.log("On SW Activation, key delete error: ", err))
+)
+});
+
+
+self.addEventListener('fetch', (e) => {
+  console.log("Fetch Listener Intercept: ", e.request);
+  // console.log();
+  e.respondWith(
+      caches
+          .match(e.request)
+          .then(response => {
+            console.log(response);
+              return response || (
+                  // We can return just this fetch req. I added the .then() so I can see the log of the server response
+                  fetch(e.request)
+                      .then(serverResponse => {
+                          console.log("Fetch Listener Server Response: ", serverResponse);
+                          return serverResponse
+                      })
+              )
+          })
+          .catch(err => {
+              console.log("On SW Fetch Intercept, response error: ", err);
+            
+              return caches.match('/offline.html')
+          })
+  )
 });
