773d59d317d1a81ed239f90116b2f5e928dd0803
Diego Hernandez <diego@thepies.co>
2024-09-07 15:13:58 -0600

    added dotenv

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import session from 'express-session';
 import morgan from 'morgan';
 import ViteExpress from 'vite-express';
 import router from './routes/index.js';
+import 'dotenv/config';
 
 const app = express();
 const port = '4090';
@@ -11,7 +12,7 @@ ViteExpress.config({ printViteDevServerHost: true });
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: false }));
+app.use(session({ secret: process.env.SESSION_SECRET, saveUninitialized: true, resave: false }));
 
 app.use(router);
 
